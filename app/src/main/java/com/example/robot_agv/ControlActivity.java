f0fdbca624351481f0f12729d4a86dd048d13051
mich-kacz <mic1999@outlook.com>
2021-01-11 16:21:53 +0100

    New version of code with working buttons and udp client to send data.

--- app/src/main/java/com/example/robot_agv/ControlActivity.java
+++ app/src/main/java/com/example/robot_agv/ControlActivity.java
@@ -1,18 +1,21 @@
 package com.example.robot_agv;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TableLayout;
-import android.widget.TextView;
+import android.widget.Toast;
+
+
 
 public class ControlActivity extends AppCompatActivity {
 
     private String IP;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +31,14 @@ public class ControlActivity extends AppCompatActivity {
         ImageButton exit=findViewById(R.id.imageButton_EXIT);
         ImageButton back=findViewById(R.id.imageButton_Back);
         ProgressBar loading=findViewById(R.id.progressBar);
+        ImageButton arrow_up=findViewById(R.id.imageButton_up);
+        ImageButton arrow_down=findViewById(R.id.imageButton_down);
+        ImageButton arrow_right=findViewById(R.id.imageButton_right);
+        ImageButton arrow_left=findViewById(R.id.imageButton_left);
+        ImageButton button_plus=findViewById(R.id.imageButton_plus);
+        ImageButton button_minus=findViewById(R.id.imageButton_minus);
+
+
 
         options.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -58,14 +69,108 @@ public class ControlActivity extends AppCompatActivity {
 
 
 
-                back.setOnClickListener(new View.OnClickListener() {
-                    public void onClick(View view) {
-                        // Do something in response to button click
-                        finish();
-                    }
+            back.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    // Do something in response to button click
+                    finish();
+                }
+            });
 
+        button_plus.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // Do something in response to button click
+                new UDP_Client(IP, "+").start();
+            }
+        });
+
+        button_minus.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // Do something in response to button click
+                new UDP_Client(IP, "-").start();
+            }
         });
 
 
+
+
+        arrow_up.setOnTouchListener((View view, MotionEvent event)->
+        {
+            super.onTouchEvent(event);
+                switch (event.getAction()) {
+                    case (MotionEvent.ACTION_DOWN ):
+                        arrow_up.setPressed(true);
+                        new UDP_Client(IP, "w").start();
+                        break;
+                    case ( MotionEvent.ACTION_UP):
+                        arrow_up.setPressed(false);
+                        new UDP_Client(IP, "k").start();
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+        });
+
+        arrow_down.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View view, MotionEvent event)
+            {
+                switch (event.getAction()) {
+                    case (MotionEvent.ACTION_DOWN ):
+                        new UDP_Client(IP, "s").start();
+                        break;
+                    case ( MotionEvent.ACTION_UP):
+                        new UDP_Client(IP, "k").start();
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
+
+
+        arrow_right.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View view, MotionEvent event)
+            {
+                switch (event.getAction()) {
+                    case (MotionEvent.ACTION_DOWN ):
+                        new UDP_Client(IP, "d").start();
+                        break;
+                    case ( MotionEvent.ACTION_UP):
+                        new UDP_Client(IP, "k").start();
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
+
+
+        arrow_left.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View view, MotionEvent event)
+            {
+                switch (event.getAction()) {
+                    case (MotionEvent.ACTION_DOWN ):
+                        new UDP_Client(IP, "a").start();
+                        break;
+                    case ( MotionEvent.ACTION_UP):
+                        new UDP_Client(IP, "k").start();
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
+
     }
+
 }
+
---
+++ app/src/main/java/com/example/robot_agv/UDP_Client.java
@@ -0,0 +1,54 @@
+package com.example.robot_agv;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class UDP_Client extends Thread
+{
+    private String IP, data_to_send;
+    public int port = 5008;
+    InetAddress IPAddress;
+
+    DatagramSocket Client_socket;
+
+    public UDP_Client(String ip, String data)
+    {
+        this.IP = ip;
+        this.data_to_send=data;
+
+    }
+
+    public void Send_data(String data) throws IOException {
+        /*Tutaj trzeba dodac cale wysyanie klawiszy w odpowiedni sposob*/
+
+        byte[] Data_b;
+
+        Data_b = data.getBytes();
+
+        Client_socket = new DatagramSocket(port);
+        IPAddress = InetAddress.getByName(IP);
+
+        DatagramPacket packet_data= new DatagramPacket(Data_b, Data_b.length, IPAddress, port);
+
+
+        Client_socket.send(packet_data);
+        System.out.println("Wysłano");
+    }
+
+    public void run()
+    {
+        try {
+            Send_data(this.data_to_send);
+            Client_socket.close();
+        } catch (IOException e) {
+            System.out.println("Błąd");
+            e.printStackTrace();
+        }
+    }
+
+}
